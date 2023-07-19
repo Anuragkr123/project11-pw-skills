ac5ee6c85002fcbafee11b89122c480af1263d51
Anuragkr123 <anuragkashyav1011@gmail.com>
2023-07-19 13:59:06 +0530

    counter app

---
+++ Counter-App/script.js
@@ -0,0 +1,28 @@
+const decrementBtn = document.getElementById("decrementBtn");
+const incrementBtn = document.getElementById("incrementBtn");
+const resetBtn = document.getElementById("resetBtn");
+const displayValue = document.getElementById("displayValue");
+
+// for decrement button click
+decrementBtn.addEventListener("click", () => {
+  const value = Number(displayValue.innerText);
+  if (value > 0) {
+    displayValue.innerText = value - 1;
+  } else {
+    alert("Negative value not allowen");
+  }
+});
+
+// for increment button click
+incrementBtn.addEventListener("click", () => {
+  const value = Number(displayValue.innerText);
+  if (value >= 100) {
+    alert("10+ values are not allowed");
+  } else {
+    displayValue.innerText = value + 1;
+  }
+});
+// for reset button click
+resetBtn.addEventListener("click", () => {
+  displayValue.innerText = 0;
+});
